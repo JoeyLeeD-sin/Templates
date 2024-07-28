3d6de77c16dac314054ef3655fdd2edd6583e596
Joseph Davidson <joseph.davidson1@sinclair.edu>
2024-07-28 18:43:30 -0400

    Added a templated function to handle the floating point values and a regular function to handle the integer values

--- Source.cpp
+++ Source.cpp
@@ -11,9 +11,27 @@
 
 using namespace std;
 
+// Templated function for floating point values
+
+template <typename T>
+typename enable_if<is_floating_point<T>::value, T>::type
+half(T value)
+{
+	return value / 2;
+}
+
+// Function to handle the integer values
+
+int half(int value)
+{
+	return static_cast<int>(round(value / 2.0));
+}
+
+
+
 int main()
 {
-	
+
 
 
 }
