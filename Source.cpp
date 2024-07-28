5f4a2a8366fadc151fc76095d2b7dd7fdafc3c1e
Joseph Davidson <joseph.davidson1@sinclair.edu>
2024-07-28 18:39:20 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+/*
+	Name: Joe Davidson
+	Date: 7/28/2024
+	Prject Name: Templates
+	Project Description: Write templated functions to perform math on varying data types
+*/
+
+#include <iostream>
+#include <cmath>
+#include <type_traits>
+
+using namespace std;
+
+int main()
+{
+	
+
+
+}
